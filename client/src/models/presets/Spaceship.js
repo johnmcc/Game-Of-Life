dea3b7f9e27624d5aaffa02a4439c046edf416ed
John McCollum <johnvmccollum@gmail.com>
2017-11-17 13:01:31 +0000

    Add the first preset

---
+++ client/src/models/presets/Spaceship.js
@@ -0,0 +1,27 @@
+var Spaceship = {
+	name: "Spaceship",
+	state: [
+		[false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+		[false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+		[true, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+		[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+		[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+		[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+		[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+		[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+		[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+		[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+		[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+		[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+		[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+		[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+		[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+		[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+		[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+		[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+		[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+		[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
+	]
+};
+
+module.exports = Spaceship;
